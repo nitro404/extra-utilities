0542c028ff228817ea5778037efde780bfcb0420
Kevin Scroggins <nitro404@gmail.com>
2018-02-10 01:44:00 -0500

    Added math helper functions and constant values

--- src/extra-utilities.js
+++ src/extra-utilities.js
@@ -6,6 +6,10 @@ if(typeof require !== "undefined") {
 
 let utilities = { };
 
+utilities.HalfPI = 1.57079632679489661923;
+utilities.QuarterPI = 0.78539816339744830962;
+utilities.TwoPI = 6.28318530717958647693;
+
 utilities.isValid = function(value) {
 	return value !== undefined && value !== null;
 };
@@ -1023,6 +1027,72 @@ utilities.matchAttribute = function(element, attribute, value) {
 	}
 
 	return element[attribute.trim()] === value;
+};
+
+utilities.clamp = function(value, min, max) {
+	return utilities.isInvalidNumber(value) || utilities.isInvalidNumber(min) || utilities.isInvalidNumber(max) ? NaN : value < min ? min : value > max ? max : value;
+};
+
+utilities.distance = function(a, b) {
+	return utilities.isInvalidNumber(a) || utilities.isInvalidNumber(b) ? NaN : Math.abs(b - a);
+};
+
+utilities.radiansToDegrees = function(value) {
+	return utilities.isInvalidNumber(value) ? NaN : value * (180 / Math.PI);
+};
+
+utilities.degreesToRadians = function(value) {
+	return utilities.isInvalidNumber(value) ? NaN : value * (Math.PI / 180);
+};
+
+utilities.compareAnglesDegrees = function(a, b) {
+	if(utilities.isInvalidNumber(a) || utilities.isInvalidNumber(b)) {
+		return NaN;
+	}
+
+	if(a == b) {
+		return 0;
+	}
+
+	let c = a % 360;
+	let d = b % 360;
+
+	if(c < 0) {
+		c += 360;
+	}
+
+	if(d < 0) {
+		d += 360;
+	}
+
+	if(c === d) {
+		return 0;
+	}
+
+	return Math.cos(utilities.degreesToRadians(a - b) + (Math.PI / 2)) < 0 ? -1 : 1;
+};
+
+utilities.compareAnglesRadians = function(a, b) {
+	return utilities.isInvalidNumber(a) || utilities.isInvalidNumber(b) ? NaN : utilities.compareAnglesDegrees(utilities.radiansToDegrees(a), utilities.radiansToDegrees(b));
+};
+
+utilities.lerp = function(a, b, amount) {
+	if(utilities.isInvalidNumber(a) || utilities.isInvalidNumber(b) || utilities.isInvalidNumber(amount)) {
+		return NaN;
+	}
+
+	if(amount === 0) {
+		return a;
+	}
+	else if(amount === 1) {
+		return b;
+	}
+
+	return a + (b - a) * amount;
+};
+
+utilities.normalize = function(value, min, max) {
+	return utilities.isInvalidNumber(value) || utilities.isInvalidNumber(min) || utilities.isInvalidNumber(max) ? NaN : (value - min) / (max - min);
 };
 
 return utilities;
